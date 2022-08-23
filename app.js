6e602514bb08d61db0a96a779577552314f4e0d1
jakewilliams33 <jakewilliams33@hotmail.com>
2022-08-23 18:50:48 +0100

    update cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 const endpoints = require("./endpoints.json");
 const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 
 const { getTopics } = require("./controllers/topics.controller");
 
@@ -17,8 +18,6 @@ const {
 const { getUsers } = require("./controllers/users.controller");
 
 const { deleteCommentById } = require("./controllers/comments.controller");
-
-app.use(cors());
 
 app.get("/api", (req, res) => res.send(endpoints));
 app.get("/api/topics", getTopics);
