7db980f992b22449d2759f4b15cc2293417b0de6
jakewilliams33 <jakewilliams33@hotmail.com>
2022-08-23 19:17:41 +0100

    change cors position

--- app.js
+++ app.js
@@ -18,19 +18,6 @@ const { getUsers } = require("./controllers/users.controller");
 const { deleteCommentById } = require("./controllers/comments.controller");
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use(express.json());
 
 app.get("/api", (req, res) => res.send(endpoints));
